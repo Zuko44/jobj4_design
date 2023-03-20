eefe7f74bcdd039b404681a5d543ff7c7c723b4e
Vladislav1992 <kuryanov.vlad@yandex.ru>
2023-03-21 00:04:07 +0300

    3. Удалить head в односвязном списке. [#51424]

---
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -0,0 +1,83 @@
+package ru.job4j.collection;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class ForwardLinked<T> implements Iterable<T> {
+    private int size = 0;
+    private int modCount = 0;
+    private Node<T> head;
+
+    public void add(T value) {
+        Node<T> node = new Node<T>(value, null);
+        if (head == null) {
+            head = node;
+            size++;
+            return;
+        }
+        Node<T> tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+        tail.next = node;
+        modCount++;
+        size++;
+    }
+
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        Node<T> target = head;
+        for (int i = 0; i < index; i++) {
+            target = target.next;
+        }
+        return target.item;
+    }
+
+    public T deleteFirst() {
+        if (head == null) {
+            throw new NoSuchElementException();
+        }
+        Node<T> next = head.next;
+        head.next = null;
+        T oldValue = head.item;
+        head = next;
+        return oldValue;
+    }
+
+    public Iterator<T> iterator() {
+        return new Iterator<>() {
+            private final int expectedModCount = modCount;
+            private Node<T> count = head;
+
+            @Override
+            public boolean hasNext() {
+                if (modCount != expectedModCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return count != null;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                T value = count.item;
+                count = count.next;
+                return value;
+            }
+        };
+    }
+
+    private static class Node<T> {
+        private T item;
+        private Node<T> next;
+
+        Node(T element, Node<T> next) {
+            this.item = element;
+            this.next = next;
+        }
+    }
+}
