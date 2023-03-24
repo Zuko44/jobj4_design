e090962ff6a76eddef9d24e4e105b02122e13387
Vladislav1992 <kuryanov.vlad@yandex.ru>
2023-03-24 20:24:44 +0300

    4. Без переопределения equals и hashCode [#1005]

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -1,6 +1,9 @@
 package ru.job4j.collection;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     private String name;
@@ -11,5 +14,27 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        Map<User, Object> map = new HashMap<>();
+        User user1 = new User("Ken", 11, new GregorianCalendar(2022, 1, 24));
+        User user2 = new User("Ken", 11, new GregorianCalendar(2022, 1, 24));
+        int hashOne = user1.hashCode();
+        int hash1 = hashOne ^ (hashOne >> 16);
+        int bucket1 = hash1 & 15;
+        /**
+         * hashOne - 1389133897 ,hash1 - 1389154949 ,bucket1 - 5
+         */
+        int hashTwo = user2.hashCode();
+        int hash2 = hashTwo ^ (hashTwo >> 16);
+        int bucket2 = hash2 & 15;
+        /**
+         * hashTwo - 1534030866 ,hash2 - 1534013309 ,bucket2 - 13
+         */
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+        System.out.println("hashOne - " + hashOne + " ,hash1 - " + hash1 + " ,bucket1 - " + bucket1);
+        System.out.print("hashTwo - " + hashTwo + " ,hash2 - " + hash2 + " ,bucket2 - " + bucket2);
     }
 }
