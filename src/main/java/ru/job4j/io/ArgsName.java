cea77c94b04e72c732abaf3cef23cc7b65cca88c
Vladislav1992 <kuryanov.vlad@yandex.ru>
2023-04-11 23:29:39 +0300

    13. Именованные аргументы [#295518]

---
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,58 @@
+package ru.job4j.io;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class ArgsName {
+    private final Map<String, String> values = new HashMap<>();
+
+    public static ArgsName of(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Arguments not passed to program");
+        }
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[]{"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[]{"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+
+    public String get(String key) {
+        String str = values.get(key);
+        if (str == null) {
+            throw new IllegalArgumentException("This key: '" + key + "' is missing");
+        }
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        for (String elem : args) {
+            validate(elem);
+            String[] str = elem.split("=", 2);
+            values.put(str[0].replace("-", ""), str[1]);
+        }
+    }
+
+    private void validate(String str) {
+        String[] str2 = str.split("=", 2);
+        if (!str.contains("=")) {
+            throw new IllegalArgumentException("Error: This argument '" + str + "' does not contain an equal sign");
+        }
+        if (str.charAt(0) != '-') {
+            throw new IllegalArgumentException("Error: This argument '" + str + "' does not start with a '-' character");
+        }
+        if (str2[1].isEmpty()) {
+            throw new IllegalArgumentException("Error: This argument '" + str + "' does not contain a value");
+        }
+        if (Objects.equals(str2[0], "-")) {
+            throw new IllegalArgumentException("Error: This argument '" + str + "' does not contain a key");
+        }
+    }
+}
