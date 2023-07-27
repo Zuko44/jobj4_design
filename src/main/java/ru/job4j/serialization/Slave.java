727213de4ed2f906c9e37f2fe77ab98e33711fff
Vladislav1992 <kuryanov.vlad@yandex.ru>
2023-07-27 19:01:25 +0300

    2. Формат JSON [#313164]

---
+++ src/main/java/ru/job4j/serialization/Slave.java
@@ -0,0 +1,30 @@
+package ru.job4j.serialization;
+
+import java.util.Arrays;
+
+public class Slave {
+    private boolean sex;
+    private int age;
+    String sobriquet;
+    private Contact contact;
+    private String[] owners;
+
+    public Slave(boolean sex, int age, String sobriquet, Contact contact, String[] owners) {
+        this.sex = sex;
+        this.age = age;
+        this.sobriquet = sobriquet;
+        this.contact = contact;
+        this.owners = owners;
+    }
+
+    @Override
+    public String toString() {
+        return "Slave{" +
+                "sex=" + sex +
+                ", age=" + age +
+                ", sobriquet='" + sobriquet + '\'' +
+                ", contact=" + contact +
+                ", owners=" + Arrays.toString(owners) +
+                '}';
+    }
+}
---
+++ src/main/java/ru/job4j/serialization/SlaveMarket.java
@@ -0,0 +1,30 @@
+package ru.job4j.serialization;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class SlaveMarket {
+    public static void main(String[] args) {
+        Slave slave = new Slave(true, 32, "blackfoot", new Contact(11111, "11-111"),
+                new String[]{"Mr Bennett", "Dicky Speck"});
+
+        Gson gson = new GsonBuilder().create();
+        System.out.println(gson.toJson(slave));
+
+        String personSlave =
+                "{"
+                        + "\"sex\":true,"
+                        + "\"age\":32,"
+                        + "\"sobriquet\":nigger,"
+                        + "\"contact\":"
+                        + "{"
+                        + "\"zipCode\":\"11111\","
+                        + "\"phone\":\"+7(924)111-111-11-11\""
+                        + "},"
+                        + "\"owners\":"
+                        + "[\"Ace Speck\",\"Calvin J. Candie\"]"
+                        + "}";
+        Slave end = gson.fromJson(personSlave, Slave.class);
+        System.out.println(end);
+    }
+}
