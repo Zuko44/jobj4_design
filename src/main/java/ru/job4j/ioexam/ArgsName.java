1131daa7702a85733c79638ad55ab4ea73f1926e
Vladislav1992 <kuryanov.vlad@yandex.ru>
2023-08-04 18:03:35 +0300

    3. Поиск файлов по критерию [#783]

---
+++ src/main/java/ru/job4j/ioexam/ArgsName.java
@@ -0,0 +1,58 @@
+package ru.job4j.ioexam;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class ArgsName {
+    private final Map<String, String> values = new HashMap<>();
+
+    public static ArgsName of(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Arguments not passed to program");
+        }
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ru.job4j.io.ArgsName jvm = ru.job4j.io.ArgsName.of(new String[]{"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ru.job4j.io.ArgsName zip = ru.job4j.io.ArgsName.of(new String[]{"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+
+    public String get(String key) {
+        String str = values.get(key);
+        if (str == null) {
+            throw new IllegalArgumentException(String.format("This key: '%s' is missing", key));
+        }
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        for (String elem : args) {
+            validate(elem);
+            String[] str = elem.split("=", 2);
+            values.put(str[0].replace("-", ""), str[1]);
+        }
+    }
+
+    private void validate(String str) {
+        String[] str2 = str.split("=", 2);
+        if (!str.contains("=")) {
+            throw new IllegalArgumentException(String.format("Error: This argument '%s' does not contain an equal sign", str));
+        }
+        if (str.charAt(0) != '-') {
+            throw new IllegalArgumentException(String.format("Error: This argument '%s' does not start with a '-' character", str));
+        }
+        if (str2[1].isEmpty()) {
+            throw new IllegalArgumentException(String.format("Error: This argument '%s' does not contain a value", str));
+        }
+        if (Objects.equals(str2[0], "-")) {
+            throw new IllegalArgumentException(String.format("Error: This argument '%s' does not contain a key", str));
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/ioexam/FileFinder.java
@@ -0,0 +1,74 @@
+package ru.job4j.ioexam;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+
+public class FileFinder {
+    public static void main(String[] args) {
+        if (args.length != 4) {
+            throw new IllegalArgumentException("arguments must be 4");
+        }
+        ArgsName argsName = ArgsName.of(args);
+        validate(argsName);
+        save(argsName);
+    }
+
+    /**
+     * Сохранение пути до найденных файлов в документ
+     */
+    public static void save(ArgsName argsName) {
+        Path path = Path.of(argsName.get("d"));
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(argsName.get("o"))
+                ))) {
+            if ("mask".equals(argsName.get("t"))) {
+                Pattern pattern = Pattern.compile(argsName.get("n"));
+                search(path, p -> pattern.matcher(p.toFile().getName()).matches()).forEach(out::println);
+            } else {
+                search(path, p -> p.toFile().getName().equals(argsName.get("n"))).forEach(out::println);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Поиск файлов с учётом типа поиска
+     */
+    public static List<Path> search(Path root, Predicate<Path> condition) {
+        SearchFiles searcher = new SearchFiles(condition);
+        try {
+            Files.walkFileTree(root, searcher);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return searcher.getPaths();
+    }
+
+    /**
+     * Валидация параметров командной строки
+     */
+    public static void validate(ArgsName args) {
+        Path path = Path.of(args.get("d"));
+        if (!Files.exists(path)) {
+            throw new IllegalArgumentException("directory does not exist");
+        }
+        if (args.get("n").length() < 1) {
+            throw new IllegalArgumentException("please, set the correct name or mask");
+        }
+        if (!("mask".equals(args.get("t")) || "name".equals(args.get("t")))) {
+            throw new IllegalArgumentException("the type of search mast be mask or name");
+        }
+        if (args.get("o").length() < 1) {
+            throw new IllegalArgumentException("wrong path");
+        }
+    }
+}
