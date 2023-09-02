5a12834a5e83ce906bded0a0fd9e9212f2f6ebbd
Vladislav1992 <kuryanov.vlad@yandex.ru>
2023-09-02 20:18:55 +0300

    0. JDBC [#6863 #441811]

--- src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -2,24 +2,32 @@ package ru.job4j.jdbc;
 
 import ru.job4j.io.Config;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionDemo {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        /**Class.forName("org.postgresql.Driver");
-         String url = "jdbc:postgresql://localhost:5432/idea_db";
-         String login = "postgres";
-         String password = "password";
-         try (Connection connection = DriverManager.getConnection(url, login, password)) {
-         DatabaseMetaData metaData = connection.getMetaData();
-         System.out.println(metaData.getUserName());
-         System.out.println(metaData.getURL());
-         }*/
+        /*Class.forName("org.postgresql.Driver");
+        String url = "jdbc:postgresql://localhost:5432/idea_db";
+        String login = "postgres";
+        String password = "password";
+        try (Connection connection = DriverManager.getConnection(url, login, password)) {
+            DatabaseMetaData metaData = connection.getMetaData();
+            System.out.println(metaData.getUserName());
+            System.out.println(metaData.getURL());
+        }*/
         Config config = new Config("data/app.properties");
         config.load();
-        System.out.println(config.value("hibernate.connection.driver_class"));
-        System.out.println(config.value("hibernate.connection.url"));
-        System.out.println(config.value("hibernate.connection.username"));
-        System.out.println(config.value("hibernate.connection.password"));
+        Class.forName(config.value("hibernate.connection.driver_class"));
+        String url = config.value("hibernate.connection.url");
+        String login = config.value("hibernate.connection.username");
+        String password = config.value("hibernate.connection.password");
+        try (Connection connection = DriverManager.getConnection(url, login, password)) {
+            DatabaseMetaData metaData = connection.getMetaData();
+            System.out.println(metaData.getUserName());
+            System.out.println(metaData.getURL());
+        }
     }
 }
